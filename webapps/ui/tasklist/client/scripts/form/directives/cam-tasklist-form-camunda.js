ffb48443534dc62d3396113de81580c3856b4474
Martin Stamm <martin.stamm@camunda.com>
2021-02-03 16:27:44 +0100

    Submit and close Task

--- webapps/ui/tasklist/client/scripts/form/directives/cam-tasklist-form-camunda.js
+++ webapps/ui/tasklist/client/scripts/form/directives/cam-tasklist-form-camunda.js
@@ -22,7 +22,6 @@ const createForm = require('camunda-forms').createForm;
 console.log('foo');
 
 var angular = require('../../../../../../camunda-commons-ui/vendor/angular');
-var $ = require('jquery');
 
 module.exports = [
   'CamForm',
@@ -52,35 +51,18 @@ module.exports = [
 
       link: function($scope, $element, attrs, formController) {
         formController.notifyFormInitialized();
-        console.log($scope);
-        console.log('element', $($element[0]).find('form')[0]);
-        const data = {};
 
-        let camForm = ($scope.camForm = null);
         var Task = camAPI.resource('task');
 
         $scope.variables = [];
 
         const loadVariables = function() {
           $scope.variablesLoaded = true;
-          Task.formVariables(
-            {
-              id: formController.getParams().taskId,
-              deserializeValues: false
-            },
-            function(err, result) {
-              if (err) {
-                $scope.variablesLoaded = false;
-                return $translate('LOAD_VARIABLES_FAILURE')
-                  .then(function(translated) {
-                    Notifications.addError({
-                      status: translated,
-                      message: err.message,
-                      scope: $scope
-                    });
-                  })
-                  .catch(angular.noop);
-              }
+          return Task.formVariables({
+            id: formController.getParams().taskId,
+            deserializeValues: false
+          })
+            .then(result => {
               angular.forEach(result, function(value, name) {
                 var parsedValue = value.value;
 
@@ -94,61 +76,51 @@ module.exports = [
                   fixedName: true
                 });
 
-                if (value.type === 'Object') {
-                  $scope.variables.push({
-                    name: name,
-                    value: value.value,
-                    type: value.type,
-                    valueInfo: value.valueInfo
-                  });
-                }
+                // if (value.type === 'Object') {
+                //   $scope.variables.push({
+                //     name: name,
+                //     value: value.value,
+                //     type: value.type,
+                //     valueInfo: value.valueInfo
+                //   });
+                // }
 
-                if (value.type === 'File') {
-                  $scope.variables.push({
-                    name: name,
-                    type: value.type,
-                    downloadUrl: Uri.appUri(
-                      'engine://engine/:engine/task/' +
-                        formController.getParams().taskId +
-                        '/variables/' +
-                        name +
-                        '/data'
-                    ),
-                    readonly: true
-                  });
-                }
+                // if (value.type === 'File') {
+                //   $scope.variables.push({
+                //     name: name,
+                //     type: value.type,
+                //     downloadUrl: Uri.appUri(
+                //       'engine://engine/:engine/task/' +
+                //         formController.getParams().taskId +
+                //         '/variables/' +
+                //         name +
+                //         '/data'
+                //     ),
+                //     readonly: true
+                //   });
+                // }
               });
-
-              console.log($scope.variables);
-            }
-          );
+            })
+            .catch(err => {
+              $scope.variablesLoaded = false;
+              return $translate('LOAD_VARIABLES_FAILURE')
+                .then(function(translated) {
+                  Notifications.addError({
+                    status: translated,
+                    message: err.message,
+                    scope: $scope
+                  });
+                })
+                .catch(angular.noop);
+            });
         };
 
-        function initializeVariables() {
-          let params = formController.getParams();
-          params = angular.copy(params);
-
-          delete params.processDefinitionKey;
-
-          angular.extend(params, {
-            client: camAPI,
-            formElement: document.createElement('form'),
-            done: () => {
-              console.log('done');
-            }
-          });
-
-          $scope.camForm = camForm = new CamForm(params);
-          camForm.variableManager;
-          loadVariables();
-        }
-
         function renderForm(schema) {
-          console.log(
-            camForm,
-            camForm.variableManager,
-            camForm.variableManager
-          );
+          const data = $scope.variables.reduce((res, variable) => {
+            res[variable.name] = variable.value;
+            return res;
+          }, {});
+
           const form = createForm({
             container: $element[0],
             schema,
@@ -156,28 +128,35 @@ module.exports = [
           });
 
           form.on('submit', event => {
-            console.log('Form <submit>', event);
+            const variablePayload = Object.entries(event.data).reduce(
+              (res, [key, value]) => {
+                res[key] = {value};
+                return res;
+              },
+              {}
+            );
+
+            Task.submitForm({
+              id: formController.getParams().taskId,
+              variables: variablePayload
+            }).then(() => {
+              formController.attemptComplete();
+            });
           });
         }
 
         function handleAsynchronousFormKey(formInfo) {
-          console.log(formInfo);
-
           fetch(formInfo.key).then(async res => {
             const json = await res.json();
 
-            initializeVariables();
+            await loadVariables();
             renderForm(json);
           });
-
-          // asynchronousFormKey = formInfo;
-          // if (formInfo && formInfo.loaded) {
-          //   showForm(container, formInfo, formController.getParams());
-          // }
-          // if (formInfo && formInfo.failure) {
-          //   formController.notifyFormInitializationFailed(formInfo.error);
-          // }
         }
+
+        formController.registerCompletionHandler(cb => {
+          cb();
+        });
 
         $scope.$watch('asynchronousFormKey', handleAsynchronousFormKey, true);
       }
